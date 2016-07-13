f9f34e57e8921587b0df7aefea57cd11db671205
Mosquit <ivos.k@atlas.cz>
2016-07-13 22:10:57 +0200

    Channel free detection done

--- app/acu_simulator.js
+++ app/acu_simulator.js
@@ -15,6 +15,14 @@ var acu_state = {
   intra_crew_status : false
 };
 
+// helper function to decide how many Services is currently in use
+function count_services() {
+  var cnt = 0;
+  Object.keys(acu_state).forEach( function (element, index, array ) {
+    if (acu_state[element] == true) cnt++;
+  });
+  return cnt;
+}
 
 app.use( express.static( __dirname + '/html' ));
 
@@ -52,14 +60,64 @@ io.on('connection', function(socket){
     io.emit('history', format_history_msg(element));
   } );
 
+
+
   socket.on('checkbox_change', function(data){
-  	console.log("Checkbox changed!!!");
-    console.log(data);
+    if(!data || !data.element) {
+      console.log ("Error while receiving Checkbox change from webapp");
+      return;
+    }
+    //console.log ("Element clicked = " + data.element + ", value  = " + data.value);
+    switch(data.element) {
+      case "sw_livepa":
+        acu_state.live_pa_status = data.value;
+        break;
+      case "sw_etw":
+        acu_state.etw_status = data.value;
+        break;
+      case "sw_ic":
+        acu_state.intra_crew_status = data.value;
+        break;
+      default:
+        console.log("Unknown checkbox element");
+        return;
+    }
+    //console.log("Now " + count_services() + " is ON, new bool value = " + data.value );
+
+    // internal state has changed, shall trigger message to OBISc
+    send_services_usage_msg();
+
+    if(data.value == true && count_services() == 1) {
+      acu_interrupted();
+    } else if (data.value == false && count_services() == 0) {
+      send_channel_msg("free");
+    }
+
   });
 
 });
 
+function send_services_usage_msg() {
+  //console.log("Sending msgs usage");
+}
 
+function acu_interrupted() {
+  //console.log ("ACU is now not prepared");
+  send_channel_msg("busy");
+
+  if(acu_state.playing == true) {
+    //current playing must be cancelled
+    send_play_interrupt_msg();
+  }
+}
+
+function send_play_interrupt_msg() {
+  console.log("Sending unsuccessful playing msg");
+}
+
+function send_channel_msg(status) {
+  console.log("Sending channel status = " + status)
+}
 
 server.listen(33333, '127.0.0.1');
 
